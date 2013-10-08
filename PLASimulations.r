974543083e365dfe00e9e77596bdfa6e5ac6149f
conorsontag <conor.sontag@gmail.com>
2013-10-07 17:35:10 -0700

    eod monday

--- PLASimulations.r
+++ PLASimulations.r
@@ -20,23 +20,26 @@ X <- pla2(2,N,-1,1)
 # those points outcome vs. f found aboev
 Y <- pla2.outcomes(X,params)
 
-# initialize wts 
-wts <- c(0,0,0)
-
 # add a vector of ones 
 X <- cbind(X,1)
 
+# initialize wts 
+wts <- c(0,0,0)
 xs <- TRUE
 iteration <- 0
+store.temp <- data.frame(iter=0,pct=0)
 while(xs){
 	iteration <- iteration + 1
 	wts <- pla2.eval(X,wts,Y)
+	wts
 	xs  <- !all(calcSign(X,wts) == Y)
+	#cat("Iteration: ",iteration,"Pct",sum(calcSign(X,wts) == Y),"\n")
+	store.temp[iteration,] <- c(iteration,sum(calcSign(X,wts) == Y))
 }
 iteration
 
 
-# Now run this experiment 1000 times and take the varage 
+# Now run this experiment 1000 times and take the average 
 i <- 1
 output.N10 <- data.frame(Trial= 1:100,Iteration = 0,Prob=0)
 for(i in 1:100){
@@ -60,7 +63,7 @@ for(i in 1:100){
 	while(xs){
 		iteration <- iteration + 1
 		wts <- pla2.eval(X,wts,Y)
-		xs  <- (!all(calcSign(X,wts) == Y) & (iteration < 1000))
+		xs  <- (!all(calcSign(X,wts) == Y) & (iteration < 150000))
 	}
 	output.N10[i,"Iteration"] <- iteration
 	
